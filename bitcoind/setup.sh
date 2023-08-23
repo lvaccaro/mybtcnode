a6dff2b8ccde28ab81af422c40e9c1b01fd801fe
lvaccaro <me@lvaccaro.com>
2023-08-23 15:43:45 +0200

    bump bitcoin core version and build instructions
    Signed-off-by: lvaccaro <me@lvaccaro.com>

--- bitcoind/setup.sh
+++ bitcoind/setup.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# build bitcoin
+git clone https://github.com/bitcoin/bitcoin/
+git checkout tags/v25.0
+cd bitcoin
+sh autogen.sh
+./configure CC=gcc-8 CXX="g++-8" LDFLAGS="-lstdc++fs" --without-bdb --disable-tests
+make
+sudo make install
+
 # setup systemd service
 cp bitcoind.service /etc/systemd/system
 systemctl daemon-reload
